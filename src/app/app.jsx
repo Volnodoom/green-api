ab5a64f3ee0dd5c06b4695eccd0cc4eccbc30d58
Alexander <bdiyoz@gmail.com>
2023-05-17 17:32:41 +0300

    [FEAT] Project initiation

---
+++ src/app/app.jsx
@@ -0,0 +1,11 @@
+import * as S from "./app.style";
+
+const App = () => {
+  return(
+    <S.AppWrapper>
+      <div>Hello</div>
+    </S.AppWrapper>
+  )
+}
+
+export default App;
---
+++ src/themes/default-theme.js
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  font: {
+    subSmall: '13px',
+    small: '15px',
+    regular: '19px',
+    regularOver: '21px',
+    medium: '27px',
+    huge: '55px',
+  },
+  color: {
+    black: '#2d3135',
+    blackText: '#232629',
+    grey: '#c0c0c0',
+    greyBorder: '#7b7b7b',
+    greyCover: '#bdbdbd',
+    white: '#ffffff',
+    whiteImpure: '#f8f9f9',
+    numberRed: '#ff0000',
+  }
+};
